887fc9b840f1b97e33da805caa639a96f4f25e50
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-12-11 20:06:33 +0100

    Clima Hecho

--- js/climasscript.js
+++ js/climasscript.js
@@ -2,6 +2,7 @@ const apiKey = "e73959ebf8294655bca200059240612"
 const city = "london"
 const ciudad = document.getElementById("ciudad")
 const climaDelDia = document.getElementById("climaDelDia")
+const pais = document.getElementById("ciudadPais")
 
 const climaFetch = async () => {
     const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&aqi=no`
@@ -10,27 +11,35 @@ const climaFetch = async () => {
         if(!response.ok){
             throw new Error ("Error en la solicitud", response.status)
         }
-
+    
         const data = await response.json();
         console.log(data);
 
+        pais.innerHTML = `
+            <p>${data.location.name}, ${data.location.country}</p>
+        ` 
+
         ciudad.innerHTML = `
-        <p>${data.location.name}, ${data.location.country}</p>
-        <img src="${data.current.condition.icon}">
-        <p>${data.current.temp_c}ºC</p>
-        <p>Viento: ${data.current.gust_kph} Km/h</p>
+            <img src="${data.current.condition.icon}">
+            <p>${data.current.temp_c}ºC</p>
+            <p>Viento: ${data.current.gust_kph} Km/h</p>
+            <p>Humedad: ${data.current.humidity}%</p>
+            <p>Precipitaciones: ${data.current.precip_in}%</p>
         `
 
         climaDelDia.innerHTML = "";
         
-        data.forEach(day => {
-            climaDelDia.innerHTML += `
-                <div>
-                    <p>${data.forecast.forecastday[0].hour}°C</p>
-                </div>
-            `;
-        })
-
+        data.forecast.forecastday.forEach(day => { // con el metodo .split("")[1] separo la linea en partes que esten separadas por un espacio dentro de los corchetes va el indice tengo que dejar espacio en las ""
+            day.hour.forEach(hour => {
+                climaDelDia.innerHTML += `
+            <div>
+                <p>${hour.time.split(" ")[1]}</p>
+                <p>${hour.temp_c}°C</p>
+                <img src="${hour.condition.icon}" alt="Icono de clima">
+            </div>
+        `;
+        });
+    })
         return data;
 
     }catch (error){
